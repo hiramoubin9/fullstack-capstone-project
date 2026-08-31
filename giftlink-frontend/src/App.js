04c8baf00c39891656bc6c1e93ad71e917405cae
hiramoubin9 <hiramoubin9@gmail.com>
2026-08-31 14:48:56 -0400

    Fix CI lint and frontend build errors

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
@@ -11,7 +11,7 @@ import SearchPage from './components/SearchPage/SearchPage';
 
 
 function App() {
-  const navigate = useNavigate();
+  
   return (
 		<>
         <Navbar/>
